9435e3c2b13c118e8ce81e8d300b90cad62ef27b
MarziSarvari <43142274+MarziSarvari@users.noreply.github.com>
2025-10-19 14:07:20 +0200

    Display watermelon data as a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_list:
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+sf_df = st.dataframe(data-smoothief root_response.json(), use_container_width=True)
